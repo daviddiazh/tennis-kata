a03dea7d2efd729f06f1a3aebb237e7eb08e5048
David Diaz <80727430+daviddiazh@users.noreply.github.com>
2021-12-12 20:10:57 -0500

    Se finaliza la refactorizacion del Game2 reduciendo los Warnings de sonarLint

--- src/main/java/TennisGame2.java
+++ src/main/java/TennisGame2.java
@@ -3,11 +3,9 @@ public class TennisGame2 implements TennisGame
 {
     public int p1Point = 0;
     public int p2Point = 0;
-    
-    public String p1Res = "";
-    public String p2Res = "";
-    private String player1Name;
-    private String player2Name;
+
+    private final String player1Name;
+    private final String player2Name;
 
     public TennisGame2(String player1Name, String player2Name) {
         this.player1Name = player1Name;
@@ -40,19 +38,12 @@ public class TennisGame2 implements TennisGame
         }
     }
 
-    public void P1Score(){
-        p1Point++;
-    }
-    
-    public void P2Score(){
-        p2Point++;
-    }
 
     public void wonPoint(String player) {
         if (player == "player1")
-            P1Score();
+            p1Point++;
         else
-            P2Score();
+            p2Point++;
     }
 
     private String scoreGreaterThan4() {
